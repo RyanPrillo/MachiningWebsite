48c711036e3a7d1853a9ee36a608c4e9cdecdad3
Ryan Prillo <rtp2930@g.rit.edu>
2024-05-24 19:24:50 -0400

    Fixed the request url for the cdn in the custom image loader. I removed a backslash, so it didn't contain 2 in a row, resulting in a miss when requesting the image

--- utils/imageLoader.js
+++ utils/imageLoader.js
@@ -1,4 +1,8 @@
+const normalizeSrc = src => {
+    return src.startsWith('/') ? src.slice(1) : src;
+};
+
 export default function cloudflareImage({src, width, quality}) {
     const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-    return `https://cdn.stellarmetalworks.com/cdn-cgi/image/${params.join(',')}${src}`
+    return `/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`
 }
