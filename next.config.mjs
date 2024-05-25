dbfb5c16dea87fea44c88056c7095571012f5c7b
Ryan Prillo <rtp2930@g.rit.edu>
2024-05-24 20:40:10 -0400

    implemented cdn
    fonts are still getting cors errors, added headers to fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
 
         return [
             {
-                source: '/public/*',
+                source: '/public/:path*',
                 locale: false,
                 headers: [
                     {
