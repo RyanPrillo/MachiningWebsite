1822e542c95e638f2f3a8264f6f8c51012b27a6c
Ryan Prillo <rtp2930@g.rit.edu>
2024-05-25 00:11:57 -0400

    Fixing next js middleware.js

--- middleware.js
+++ middleware.js
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
     const url = request.nextUrl.clone();
+    console.log("URL HERE:", url, "HOSTNAME HERE:", url.hostname, "TEST:", url.pathname.indexOf("/_next/static/media") > -1, "REWRITE:", NextResponse.rewrite(url))
     if (url.pathname.indexOf("/_next/static/media") > -1) {
         url.hostname = url.hostname.replace("cdn.", "")
-        console.log(url, url.hostname, url.pathname.indexOf("/_next/static/media") > -1, NextResponse.rewrite(url))
     }
     return NextResponse.rewrite(url);
 }
