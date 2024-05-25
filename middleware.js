9d9f284d6ec6200552b5ef4a1ecf92acab78a6b4
Ryan Prillo <rtp2930@g.rit.edu>
2024-05-24 23:54:15 -0400

    Added middleware.js to rewrite the url of any files in /_next/static/media (fonts), removing the cdn prefix

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
     const url = request.nextUrl.clone();
-    if (url.pathname.startsWith("/_next/static/media")) {
+    if (url.pathname.indexOf("/_next/static/media") > -1) {
         url.hostname = url.hostname.replace("cdn.", "")
     }
     return NextResponse.rewrite(url);
