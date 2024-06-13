0cde6230fd746ac6f53b66f7440ca119af800ec9
Ryan Prillo <rtp2930@g.rit.edu>
2024-06-13 17:34:50 -0400

    Fixing Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import {manrope} from "@/app/fonts";
 import "./globals.css";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL(`https://www.stellarmetalworks.com`),
@@ -16,11 +16,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}></Script>
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </head>
     {/*Use manrope font across the site*/}
     <body className={manrope.className}>
     <link rel="icon" href="/favicon-32x32.png" sizes="any"/>
     {children}
-    <GoogleAnalytics />
     </body>
     </html>
   );
