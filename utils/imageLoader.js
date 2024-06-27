92b57e49cd9195f98cfee4145f131844cee5dd95
Ryan Prillo <rtp2930@g.rit.edu>
2024-06-26 20:17:03 -0400

    fixing image custom loader

--- utils/imageLoader.js
+++ utils/imageLoader.js
@@ -5,5 +5,6 @@ const normalizeSrc = src => {
 
 export default function cloudflareImage({src, width, quality}) {
     const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
+    console.log(`https://cdn.stellarmetalworks.com/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`)
     return `https://cdn.stellarmetalworks.com/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`
 }
